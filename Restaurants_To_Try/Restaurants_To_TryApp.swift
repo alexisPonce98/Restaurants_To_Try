f1225c2852f2f3c9cf695d9c23b6d163927d85ef
Alexis Ponce <ponce.alexis98@gmail.com>
2022-05-10 16:30:06 -0700

    Initial Commit

---
+++ Restaurants_To_Try/Restaurants_To_TryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurants_To_TryApp.swift
+//  Restaurants_To_Try
+//
+//  Created by Alexis Ponce on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Restaurants_To_TryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
